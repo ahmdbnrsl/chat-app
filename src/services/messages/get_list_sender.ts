6838e4ebbdb0e39b499ee9fa601bc03ec5f7c909
abrazax56 <ahmadbenirusli@gmail.com>
2024-08-13 06:39:37 +0700

    chat app: fix performance of get list sender

--- src/services/messages/get_list_sender.ts
+++ src/services/messages/get_list_sender.ts
@@ -16,69 +16,89 @@ export const getListSender = async ({
     user_id
 }: {
     user_id: string;
-}): Promise<Array<Result> | false> => {
+}): Promise<
+    { result?: Array<Result>; status: boolean; message: string } | false
+> => {
     try {
-        const list: Array<Message> = await messages.find({
-            $or: [{ sender_id: user_id }, { receiver_id: user_id }]
+        const checkExistingUser: User | null = await users.findOne({
+            user_id
         });
-        let uniqueSenders: Set<string> = new Set();
-        let uniqueReceivers: Set<string> = new Set();
-        let senderArray: string[] = [];
-        let receiverArray: string[] = [];
+        if (checkExistingUser) {
+            const list: Array<Message> = await messages.find({
+                $or: [{ sender_id: user_id }, { receiver_id: user_id }]
+            });
+            let uniqueSenders: Set<string> = new Set();
+            let uniqueReceivers: Set<string> = new Set();
+            let senderArray: string[] = [];
+            let receiverArray: string[] = [];
 
-        list.forEach((item: Message) => {
-            if (!uniqueSenders.has(item.sender_id)) {
-                uniqueSenders.add(item.sender_id);
-                senderArray.push(item.sender_id);
-            }
-            if (!uniqueReceivers.has(item.receiver_id)) {
-                uniqueReceivers.add(item.receiver_id);
-                receiverArray.push(item.receiver_id);
-            }
-        });
+            list.forEach((item: Message) => {
+                if (!uniqueSenders.has(item.sender_id)) {
+                    uniqueSenders.add(item.sender_id);
+                    senderArray.push(item.sender_id);
+                }
+                if (!uniqueReceivers.has(item.receiver_id)) {
+                    uniqueReceivers.add(item.receiver_id);
+                    receiverArray.push(item.receiver_id);
+                }
+            });
 
-        const listData: string[] = [...senderArray, ...receiverArray];
-        let index: number = listData.indexOf(user_id);
-        if (index !== -1) {
-            listData.splice(index, 1);
-        }
-        if (listData.length === 0) {
-            return [];
-        }
-        const userData: Array<{ user_id: string }> = listData.map(
-            (user_id: string) => {
+            const listData: string[] = [...senderArray, ...receiverArray];
+            let index: number = listData.indexOf(user_id);
+            if (index !== -1) {
+                listData.splice(index, 1);
+            }
+            if (listData.length === 0) {
                 return {
-                    user_id
+                    result: [],
+                    status: true,
+                    message: 'no sender found'
                 };
             }
-        );
-        const userss: Array<User> = await users.find({ $or: userData });
-        const listSender: Array<Promise<Result>> = userss.map(
-            async (user: User) => {
-                const message: Array<Message> = await messages.find({
-                    $or: [
-                        { sender_id: user_id, receiver_id: user?.user_id },
-                        { sender_id: user?.user_id, receiver_id: user_id }
-                    ]
-                });
-                const latestMessage: Message = message.reduce(
-                    (prev: Message, current: Message) =>
-                        Number(prev.message_timestamp) >
-                        Number(current.message_timestamp)
-                            ? prev
-                            : current
-                );
-                return {
-                    pp: user?.pp,
-                    name: user?.name,
-                    wa_number: user?.wa_number,
-                    latestMessageText: latestMessage?.message_text,
-                    latestMessageTimestamp: latestMessage?.message_timestamp
-                };
-            }
-        );
+            const userData: Array<{ user_id: string }> = listData.map(
+                (user_id: string) => {
+                    return {
+                        user_id
+                    };
+                }
+            );
+            const userss: Array<User> = await users.find({ $or: userData });
+            const listSender: Array<Promise<Result>> = userss.map(
+                async (user: User) => {
+                    const message: Array<Message> = await messages.find({
+                        $or: [
+                            { sender_id: user_id, receiver_id: user?.user_id },
+                            { sender_id: user?.user_id, receiver_id: user_id }
+                        ]
+                    });
+                    const latestMessage: Message = message.reduce(
+                        (prev: Message, current: Message) =>
+                            Number(prev.message_timestamp) >
+                            Number(current.message_timestamp)
+                                ? prev
+                                : current
+                    );
+                    return {
+                        pp: user?.pp,
+                        name: user?.name,
+                        wa_number: user?.wa_number,
+                        latestMessageText: latestMessage?.message_text,
+                        latestMessageTimestamp: latestMessage?.message_timestamp
+                    };
+                }
+            );
 
-        return Promise.all(listSender);
+            return {
+                result: Promise.all(listSender),
+                status: true,
+                message: 'success get list sender'
+            };
+        } else {
+            return {
+                status: false,
+                message: 'user is not registered'
+            };
+        }
     } catch (error) {
         return false;
     }
