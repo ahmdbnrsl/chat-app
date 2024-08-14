16f4059c815d205053929e30563e7054cd2dd684
abrazax56 <ahmadbenirusli@gmail.com>
2024-08-14 20:43:13 +0700

    chat app: add route chat room

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,7 +2,6 @@
 import { Inter } from 'next/font/google';
 import { usePathname } from 'next/navigation';
 import { SessionProvider } from 'next-auth/react';
-import Navbar from './navbar';
 import './globals.css';
 
 const inter = Inter({ subsets: ['latin'] });
@@ -18,10 +17,7 @@ export default function RootLayout({
     return (
         <html lang='en'>
             <body className={inter.className}>
-                <SessionProvider>
-                    {!disableNavbar.includes(pathName) && <Navbar />}
-                    {children}
-                </SessionProvider>
+                <SessionProvider>{children}</SessionProvider>
             </body>
         </html>
     );
