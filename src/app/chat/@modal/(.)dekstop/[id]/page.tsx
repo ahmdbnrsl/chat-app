47e30a70eade4a545172a834c8d243ade133470c
abrazax56 <ahmadbenirusli@gmail.com>
2024-08-14 20:49:53 +0700

    chat app: add route chat room

--- src/app/chat/@modal/(.)dekstop/[id]/page.tsx
+++ src/app/chat/@modal/(.)dekstop/[id]/page.tsx
@@ -19,7 +19,7 @@ export default function DekstopView(props: any) {
             onClick={close}
         >
             <div className='absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 p-6 bg-white rounded-lg text-black'>
-                {params.id}
+                <h1> chat with : {params.id}</h1>
             </div>
         </div>
     );
