193c02e0aa3b5c9aad683c9ee732647627d733d6
abrazax56 <ahmadbenirusli@gmail.com>
2024-08-14 20:57:00 +0700

    chat app: add route chat room

--- src/app/chat/@modal/(.)dekstop/[id]/page.tsx
+++ src/app/chat/@modal/(.)dekstop/[id]/page.tsx
@@ -15,7 +15,7 @@ export default function DekstopView(props: any) {
     return (
         <div
             ref={overlay}
-            className='fixed z-10 left-0 right-0 top-0 bottom-0 mx-auto bg-black/60'
+            className='fixed z-[999] left-0 right-0 top-0 bottom-0 mx-auto bg-black/60'
             onClick={close}
         >
             <div className='absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 p-6 bg-white rounded-lg text-black'>
