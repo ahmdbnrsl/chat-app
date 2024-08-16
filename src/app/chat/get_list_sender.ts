b79dede54698f8ee5c9a68f58ff0d59d5bc4b30c
abrazax56 <ahmadbenirusli@gmail.com>
2024-08-16 07:06:09 +0700

    chat app: add short polling to fetch message and sender list

--- src/app/chat/get_list_sender.ts
+++ src/app/chat/get_list_sender.ts
@@ -24,9 +24,7 @@ export const getListSender = async (
                 user_id,
                 secret: process.env.NEXT_PUBLIC_SECRET
             }),
-            next: {
-                revalidate: 5
-            }
+            cache: 'no-store'
         };
         const response: Response = await fetch(
             'https://chat-app-rouge-alpha.vercel.app/api/get_list_sender',
