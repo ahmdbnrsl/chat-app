7a9e17b3437e8dcb99b564493ed69b04d29f7b65
abrazax56 <ahmadbenirusli@gmail.com>
2024-08-16 21:17:05 +0700

    chat app: fix home page chat get list sender

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -1,11 +1,5 @@
 import NextAuth from 'next-auth/next';
 import { authOptions } from '@/lib/auth';
 
-interface Result {
-    user?: User | null;
-    status: boolean;
-    message: string;
-}
-
 const handler = NextAuth(authOptions);
 export { handler as GET, handler as POST };
