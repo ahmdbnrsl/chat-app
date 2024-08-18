93e022430acdd9947fae76ee5b3fbeb83161c5b5
abrazax56 <ahmadbenirusli@gmail.com>
2024-08-18 15:05:00 +0700

    chat app: re-optimizing

---
+++ src/app/chat/layout.tsx
@@ -0,0 +1,123 @@
+'use client';
+import { useSession } from 'next-auth/react';
+import Avatar from 'react-avatar';
+import useSWR from 'swr';
+import Image from 'next/image';
+import Link from 'next/link';
+import Loading from '@/components/loading';
+import Wrapper from './wrapper';
+
+interface Result {
+    pp: string;
+    name: string;
+    wa_number: string;
+    fromMe: boolean;
+    latestMessageText: string;
+    latestMessageTimestamp: string;
+    id_user: string;
+}
+
+export default function Layout({ children }: { children: React.ReactNode }) {
+    const { data: session, status }: { data: any; status: string } =
+        useSession();
+    const user_id: string | undefined | null = session?.user?.user_id;
+    let listSender: Array<Result> | undefined = undefined;
+    const options: RequestInit = {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+            user_id
+        }),
+        cache: 'no-store'
+    };
+    const fetcher = (url: string) =>
+        fetch(url, options).then(res => res.json());
+    const { data, error, isLoading } = useSWR(
+        'https://chat-app-rouge-alpha.vercel.app/api/get_list_sender',
+        fetcher
+    );
+    listSender = data?.result;
+    const getTimestamp = (isDate: string): string => {
+        const date: Date = new Date(Number(isDate));
+        return `${date.getFullYear()}-${String(date.getMonth() + 1).padStart(
+            2,
+            '0'
+        )}-${String(date.getDate()).padStart(2, '0')} ${String(
+            date.getHours()
+        ).padStart(2, '0')}:${String(date.getMinutes()).padStart(
+            2,
+            '0'
+        )}:${String(date.getSeconds()).padStart(2, '0')}`;
+    };
+    const linkTo: string =
+        window.innerWidth < 1280 ? '/chat/mobile' : '/chat/dekstop';
+
+    return (
+        <main className='bg-zinc-950 w-full min-h-screen flex'>
+            <Wrapper>
+                <div className='w-full flex flex-col gap-3 p-6 flex-grow'>
+                    {!listSender ? (
+                        <div className='w-full flex justify-center gap-1.5 items-center text-lg font-medium text-zinc-500'>
+                            <Loading /> Loading your chats...
+                        </div>
+                    ) : listSender?.length !== 0 ? (
+                        listSender?.map((sender: Result, index: number) => (
+                            <Link
+                                href={`${linkTo}/${sender?.id_user}`}
+                                key={index}
+                                className='w-full rounded-xl transition-colors hover:bg-zinc-900/[0.85] flex justify-between items-center p-3'
+                            >
+                                <div className='w-8/12 flex gap-3 items-center'>
+                                    {sender?.pp !== 'empety' ? (
+                                        <Image
+                                            alt='User profile'
+                                            src={sender?.pp}
+                                            height={50}
+                                            width={50}
+                                            className='rounded-full'
+                                            loading='lazy'
+                                        />
+                                    ) : (
+                                        <Avatar
+                                            name={sender?.name}
+                                            size='50'
+                                            round={true}
+                                        />
+                                    )}
+                                    <div className='flex flex-col'>
+                                        <h1 className='text-base sm:text-lg text-zinc-300 font-normal'>
+                                            {sender?.name}
+                                        </h1>
+                                        <p className='w-[15ch] sm:w-[25ch] md:w-[50ch] xl:w-[15ch] text-xs sm:text-sm text-zinc-400 truncate'>
+                                            {sender?.fromMe ? 'You : ' : ''}
+                                            {sender?.latestMessageText}
+                                        </p>
+                                    </div>
+                                </div>
+                                <div className='w-1/4 flex flex-col justify-center items-end'>
+                                    <p className='text-xs sm:text-sm font-normal text-zinc-400'>
+                                        +{sender?.wa_number}
+                                    </p>
+                                    <p className='text-xs font-normal text-zinc-500'>
+                                        {getTimestamp(
+                                            sender?.latestMessageTimestamp
+                                        )}
+                                    </p>
+                                </div>
+                            </Link>
+                        ))
+                    ) : (
+                        <div className='w-full flex justify-center gap-1.5 items-center text-lg font-medium text-zinc-500'>
+                            No chat found
+                        </div>
+                    )}
+                </div>
+            </Wrapper>
+            <section className='hidden w-full xl:flex flex-col min-h-screen xl:w-4/6 bg-zinc-950 bg-ornament bg-fixed'>
+                {children}
+            </section>
+        </main>
+    );
+}
--- src/app/chat/wrapper.tsx
+++ src/app/chat/wrapper.tsx
@@ -9,7 +9,7 @@ export default function Wrapper({ children }: { children: React.ReactNode }) {
     const { data: session, status }: { data: any; status: string } =
         useSession();
     return (
-        <main className='bg-zinc-900 w-full min-h-screen flex'>
+        <>
             {showModal && (
                 <div className='justify-center items-center flex overflow-x-hidden overflow-y-auto fixed inset-0 z-50 outline-none focus:outline-none bg-zinc-500/[0.15] backdrop-blur p-4'>
                     <div
@@ -24,8 +24,8 @@ export default function Wrapper({ children }: { children: React.ReactNode }) {
                     />
                 </div>
             )}
-            <section className='w-full flex flex-col min-h-screen xl:w-1/3'>
-                <nav className='sticky top-0 z-20 bg-zinc-900 w-full py-4 px-6 flex flex-col border-b border-zinc-800 items-center'>
+            <section className='bg-zinc-950 w-full flex flex-col min-h-screen xl:w-1/3'>
+                <nav className='sticky top-0 z-20 bg-zinc-950 w-full py-4 px-6 flex flex-col border-b border-zinc-800 items-center'>
                     <div className='flex justify-between w-full items-center'>
                         <h1 className='text-zinc-200 text-lg sm:text-xl font-semibold tracking-normal'>
                             Chats
@@ -55,16 +55,16 @@ export default function Wrapper({ children }: { children: React.ReactNode }) {
                     <input
                         type='text'
                         placeholder='Search by name'
-                        className='mt-2 w-full py-2 px-4 text-zinc-300 font-normal text-base sm:text-lg rounded-lg bg-zinc-800/[0.5] outline-0 border-2 border-zinc-800 focus:border-zinc-700 placeholder:text-zinc-400 placeholder:text-sm sm:placeholder:text-base'
+                        className='mt-2 w-full py-2 px-4 text-zinc-300 font-normal text-base sm:text-lg rounded-lg bg-zinc-900/[0.5] outline-0 border-2 border-zinc-800 focus:border-zinc-700 placeholder:text-zinc-400 placeholder:text-sm sm:placeholder:text-base'
                     />
                 </nav>
                 {children}
-                <div className='sticky bottom-0 bg-zinc-900 w-full py-4 px-6 flex flex-col border-t border-zinc-800 items-center'>
+                <div className='sticky bottom-0 bg-zinc-950 w-full py-4 px-6 flex flex-col border-t border-zinc-800 items-center'>
                     <button className='bg-zinc-200 rounded-full py-1.5 px-6 outline-0 text-zinc-950 text-center'>
                         + Start Chat
                     </button>
                 </div>
             </section>
-        </main>
+        </>
     );
 }
