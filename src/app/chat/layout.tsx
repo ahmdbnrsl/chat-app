7c7384b2c30c92960e3e2fee79ef6742213fa2c9
abrazax56 <ahmadbenirusli@gmail.com>
2024-08-19 08:27:30 +0700

    chat app: add web socket to layout chat

--- src/app/chat/layout.tsx
+++ src/app/chat/layout.tsx
@@ -92,7 +92,69 @@ export default function Layout({ children }: { children: React.ReactNode }) {
     }, []);
 
     if (width < 1280) {
-        return <>{children}</>;
+        return (
+            <main className='bg-zinc-950 w-full min-h-screen flex'>
+                <Wrapper>
+                    <div className='w-full flex flex-col gap-3 p-6 flex-grow'>
+                        {!listSender ? (
+                            <div className='w-full flex justify-center gap-1.5 items-center text-lg font-medium text-zinc-500'>
+                                <Loading /> Loading your chats...
+                            </div>
+                        ) : listSender?.length !== 0 ? (
+                            listSender?.map((sender: Result, index: number) => (
+                                <Link
+                                    href={`/user_id/${sender?.id_user}`}
+                                    key={index}
+                                    className='w-full rounded-xl transition-colors hover:bg-zinc-900/[0.85] flex justify-between items-center p-3'
+                                >
+                                    <div className='w-8/12 flex gap-3 items-center'>
+                                        {sender?.pp !== 'empety' ? (
+                                            <Image
+                                                alt='User profile'
+                                                src={sender?.pp}
+                                                height={50}
+                                                width={50}
+                                                className='rounded-full'
+                                                loading='lazy'
+                                            />
+                                        ) : (
+                                            <Avatar
+                                                name={sender?.name}
+                                                size='50'
+                                                round={true}
+                                            />
+                                        )}
+                                        <div className='flex flex-col'>
+                                            <h1 className='text-base sm:text-lg text-zinc-300 font-normal'>
+                                                {sender?.name}
+                                            </h1>
+                                            <p className='w-[15ch] sm:w-[25ch] md:w-[50ch] xl:w-[15ch] text-xs sm:text-sm text-zinc-400 truncate'>
+                                                {sender?.fromMe ? 'You : ' : ''}
+                                                {sender?.latestMessageText}
+                                            </p>
+                                        </div>
+                                    </div>
+                                    <div className='w-1/4 flex flex-col justify-center items-end'>
+                                        <p className='text-xs sm:text-sm font-normal text-zinc-400'>
+                                            +{sender?.wa_number}
+                                        </p>
+                                        <p className='text-xs font-normal text-zinc-500'>
+                                            {getTimestamp(
+                                                sender?.latestMessageTimestamp
+                                            )}
+                                        </p>
+                                    </div>
+                                </Link>
+                            ))
+                        ) : (
+                            <div className='w-full flex justify-center gap-1.5 items-center text-lg font-medium text-zinc-500'>
+                                No chat found
+                            </div>
+                        )}
+                    </div>
+                </Wrapper>
+            </main>
+        );
     }
 
     return (
