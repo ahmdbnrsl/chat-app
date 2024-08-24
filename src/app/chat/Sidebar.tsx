3527df042eae4344c45a339ad602014ba5b7a9fb
abrazax56 <ahmadbenirusli@gmail.com>
2024-08-24 07:38:25 +0700

    chat app: redesign home chat

--- src/app/chat/Sidebar.tsx
+++ src/app/chat/Sidebar.tsx
@@ -19,17 +19,32 @@ export default function SidebarChat({
 }: {
     listSender: Array<Result> | null | undefined;
 }) {
-    const getTimestamp = (isDate: string): string => {
+    const getHour = (isDate: string): string => {
         const date: Date = new Date(Number(isDate));
-        return `${date.getFullYear()}-${String(date.getMonth() + 1).padStart(
-            2,
-            '0'
-        )}-${String(date.getDate()).padStart(2, '0')} ${String(
-            date.getHours()
-        ).padStart(2, '0')}:${String(date.getMinutes()).padStart(
-            2,
-            '0'
-        )}:${String(date.getSeconds()).padStart(2, '0')}`;
+        return `${String(date.getHours()).padStart(2, '0')}:${String(
+            date.getMinutes()
+        ).padStart(2, '0')}`;
+    };
+    const getDate = (isDate: string): string => {
+        const date: Date = new Date(Number(isDate));
+        const monthNames: string[] = [
+            'January',
+            'February',
+            'March',
+            'April',
+            'May',
+            'June',
+            'July',
+            'August',
+            'September',
+            'October',
+            'November',
+            'December'
+        ];
+        const monthName: string = monthNames[date.getMonth()];
+        return `${date.getFullYear()} ${monthName} ${String(
+            date.getDate()
+        ).padStart(2, '0')}`;
     };
     return (
         <div className='w-full flex flex-col gap-3 p-6 flex-grow overflow-y-auto max-h-screen'>
@@ -76,7 +91,10 @@ export default function SidebarChat({
                                 +{sender?.wa_number}
                             </p>
                             <p className='text-xs font-normal text-zinc-500'>
-                                {getTimestamp(sender?.latestMessageTimestamp)}
+                                {getDate(latestMessageTimestamp) ===
+                                getDate(Date.now().toString())
+                                    ? getHour(latestMessageTimestamp)
+                                    : getDate(latestMessageTimestamp)}
                             </p>
                         </div>
                     </Link>
