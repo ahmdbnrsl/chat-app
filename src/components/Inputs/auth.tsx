a56d7ed3f698efb3250a0c1a42d08388938b0292
abrazax56 <ahmadbenirusli@gmail.com>
2024-08-29 08:07:08 +0700

    chat app: create input and labels component

--- src/components/Inputs/auth.tsx
+++ src/components/Inputs/auth.tsx
@@ -15,7 +15,9 @@ export default function AuthInput({
             name={identifier}
             maxLength={maxs}
             placeholder='...'
-            className={`${identifier} peer w-full bg-zinc-900 outline-0 text-lg font-normal text-zinc-200 tracking-wider rounded-xl px-4 py-2 border-4 border-zinc-600 placeholder:text-transparent focus:border-zinc-400 cursor-text`}
+            className={`${identifier} ${
+                identifier === 'otp' && 'text-center'
+            } peer w-full bg-zinc-900 outline-0 text-lg font-normal text-zinc-200 tracking-wider rounded-xl px-4 py-2 border-4 border-zinc-600 placeholder:text-transparent focus:border-zinc-400 cursor-text`}
         />
     );
 }
