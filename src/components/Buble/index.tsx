262573a23dabb0ec18e5fe21df7b4668e4af7dee
abrazax56 <ahmadbenirusli@gmail.com>
2024-09-02 15:08:01 +0700

    chat-app: add reply message feature

--- src/components/Buble/index.tsx
+++ src/components/Buble/index.tsx
@@ -16,26 +16,43 @@ export default function BubleMessage({
     isFromMe: boolean;
 }) {
     const timestamp = useCallback(hour, []);
+    const HandleScroll = (e: MouseEvent<HTMLDivElement>, id: string) => {
+        e.preventDefault();
+        const el: HTMLElement | null = document.getElementById(id);
+        if (el !== null || el) {
+            el.scrollIntoView({
+                block: 'center',
+                behavior: 'smooth'
+            });
+            el.style.transform = 'scale(1.05)';
+            setTimeout(() => {
+                el.style.transform = 'scale(1)';
+            }, 1500);
+        }
+    };
     return (
         <div
             onClick={onClicking}
             key={key}
             id={buble.message_id}
-            className={`w-fit h-fit ${
+            className={`w-fit h-fit transition-transform ${
                 isFromMe ? 'bg-slate-900' : 'bg-zinc-900'
             } rounded-xl py-2 text-base px-3 text-zinc-300 min-w-[5rem] flex flex-col`}
         >
             {buble?.message_quoted && (
-                <div className='w-full p-1'>
+                <div className='w-full px-[0.06rem] pt-[0.06rem] pb-1.5'>
                     <div
-                        className={`w-full p-2 flex flex-col gap-1.5 rounded-xl ${
+                        onClick={e =>
+                            HandleScroll(e, buble.message_quoted.message_id)
+                        }
+                        className={`w-full p-2 flex flex-col gap-1 rounded-xl ${
                             isFromMe ? 'bg-zinc-900' : 'bg-slate-900'
                         }`}
                     >
                         <p className='text-zinc-400 text-sm font-medium'>
                             {buble.message_quoted.from_name}
                         </p>
-                        <p className='text-zinc-500 text-xs font-normal'>
+                        <p className='text-zinc-500 text-xs font-normal truncate'>
                             {buble.message_quoted.message_text}
                         </p>
                     </div>
