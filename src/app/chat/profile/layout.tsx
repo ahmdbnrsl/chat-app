f9b30d6a776bb2037886d22d75eebd4748547a3d
abrazax56 <ahmadbenirusli@gmail.com>
2024-09-06 05:59:57 +0700

    chat-app: re-layouting profile page

--- src/app/chat/profile/layout.tsx
+++ src/app/chat/profile/layout.tsx
@@ -28,8 +28,8 @@ export default function EditLayout({
     children: React.ReactNode;
 }>) {
     return (
-        <main className='transition-all w-full min-h-screen bg-zinc-950 bg-ornament bg-[length:500px]'>
-            <section className='w-full min-h-screen p-5 flex flex-col justify-center items-center'>
+        <main className='transition-all w-full min-h-screen bg-zinc-900 md:bg-zinc-950 md:bg-ornament md:bg-[length:500px]'>
+            <section className='w-full min-h-screen md:p-5 flex flex-col md:justify-center md:items-center'>
                 {children}
             </section>
         </main>
