6ccba139cae8ba8b852e09a0df334d270fe12633
abrazax56 <ahmadbenirusli@gmail.com>
2024-09-05 19:59:01 +0700

    chat-app: add search message feature

--- src/components/Inputs/search.tsx
+++ src/components/Inputs/search.tsx
@@ -14,18 +14,18 @@ export default function SearchInput({
     isSearchMessage?: boolean;
 }) {
     return (
-        <div className='w-full flex justify-center items-center bg-zinc-900/[0.5] focus:bg-zinc-900 rounded-lg'>
-            <div className='-mb-[0.2rem] text-zinc-500 text-lg sm:text-xl'>
+        <div className='mt-2 md:mt-0 px-3 w-full flex justify-center items-center rounded-lg'>
+            <div className='-mb-[0.2rem] text-zinc-500 text-xl sm:text-2xl'>
                 <IoSearch />
             </div>
             <input
                 type='text'
                 onChange={onChanging || (() => {})}
                 placeholder={placeHolder}
-                className='w-full p-2 px-4 text-zinc-300 font-normal text-base sm:text-lg placeholder:text-zinc-500 placeholder:text-sm sm:placeholder:text-base'
+                className='bg-zinc-900/[0.5] focus:bg-zinc-900 w-full p-2 px-4 text-zinc-300 font-normal text-base sm:text-lg placeholder:text-zinc-500 placeholder:text-sm sm:placeholder:text-base outline-0'
             />
-            {isSearchMessage ?? (
-                <button className='bg-transparent outline-0 text-zinc-500 text-lg sm:text-xl'>
+            {isSearchMessage && (
+                <button className='bg-transparent outline-0 text-zinc-500 text-xl sm:text-2xl'>
                     <MdOutlineDateRange />
                 </button>
             )}
