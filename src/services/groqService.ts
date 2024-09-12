1fe915531927b87d0b85f479735b3a5e0ea0f021
abrazax56 <ahmadbenirusli@gmail.com>
2024-09-12 20:55:32 +0700

    chat-app: add server action for request to groq ai

---
+++ src/services/groqService.ts
@@ -0,0 +1,17 @@
+'use server';
+
+import Groq from 'groq-sdk';
+
+export async function requestChatCompletions(content: string): Promise<string> {
+    const groq = new Groq({ apiKey: process.env.GROQ_API_KEY || '' });
+    const result = groq.chat.completions.create({
+        messages: [
+            {
+                role: 'user',
+                content
+            }
+        ],
+        model: 'llama3-70b-8192'
+    });
+    return chatCompletion.choices[0]?.message?.content || '';
+}
