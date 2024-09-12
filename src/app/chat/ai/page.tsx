e31ad04eddf078fdbaebbdf697351550c9e1d10f
abrazax56 <ahmadbenirusli@gmail.com>
2024-09-12 07:46:21 +0700

    chat-app: prepare to setup ai chat

--- src/app/chat/ai/page.tsx
+++ src/app/chat/ai/page.tsx
@@ -5,47 +5,29 @@ import LoadingMessage from '@/components/loading';
 import { BsStars } from 'react-icons/bs';
 
 export default function AiChats() {
-    const listMessage: string[] = [];
     return (
         <main className='bg-zinc-950 w-full min-h-screen flex'>
-            {!listMessage ? (
-                <section className='w-full flex flex-col h-screen bg-fixed bg-zinc-950 justify-center items-center'>
-                    <h1 className='flex items-center gap-2 sm:gap-3 text-lg sm:text-xl md:text-2xl text-zinc-400 font-medium text-center scale-[3]'>
-                        <LoadingMessage />
+            <section className='w-full flex flex-col h-screen bg-ornament bg-fixed bg-zinc-950 overflow-y-auto p-6'>
+                <div className='w-full flex flex-col justify-center items-center p-6 flex-grow bg-zinc-950'>
+                    <Image
+                        src='https://uxwing.com/wp-content/themes/uxwing/download/brands-and-social-media/google-gemini-icon.png'
+                        width={100}
+                        height={100}
+                        alt='user profile'
+                        loading='lazy'
+                        className='rounded-full'
+                    />
+                    <h1 className='mt-2 flex items-center gap-2 sm:gap-3 text-lg sm:text-xl md:text-2xl text-indigo-400 font-bold text-center'>
+                        VB AI <BsStars className='ml-2' />
                     </h1>
-                </section>
-            ) : (
-                <section className='w-full flex flex-col h-screen bg-ornament bg-fixed bg-zinc-950 overflow-y-auto'>
-                    {listMessage.length !== 0 ? (
-                        <>
-                            <nav className='sticky top-0 z-20 bg-zinc-950 w-full py-4 px-6 flex flex-col md:flex-row gap-3 border-b border-zinc-800 items-center justify-center text-xl font-bold text-indigo-400'>
-                                VB AI <BsStars className='ml-2' />
-                            </nav>
-                            <div className='w-full flex flex-col flex-grow justify-end'></div>
-                        </>
-                    ) : (
-                        <div className='w-full flex flex-col justify-center items-center p-6 flex-grow bg-zinc-950'>
-                            <Image
-                                src='https://uxwing.com/wp-content/themes/uxwing/download/brands-and-social-media/google-gemini-icon.png'
-                                width={100}
-                                height={100}
-                                alt='user profile'
-                                loading='lazy'
-                                className='rounded-full'
-                            />
-                            <h1 className='mt-2 flex items-center gap-2 sm:gap-3 text-lg sm:text-xl md:text-2xl text-indigo-400 font-bold text-center'>
-                                VB AI <BsStars className='ml-2' />
-                            </h1>
-                            <button
-                                type='button'
-                                className='mt-2 bg-gradient-to-br from-indigo-200 to-fuchsia-100 rounded-full py-1.5 px-6 outline-0 text-zinc-950 text-center cursor-pointer hover:scale-[1.025] transition-transform'
-                            >
-                                Start Chatting
-                            </button>
-                        </div>
-                    )}
-                </section>
-            )}
+                    <form className='mt-3 w-full max-w-xl'>
+                        <textarea
+                            placeholder='Ask something'
+                            className='w-full bg-transparent text-base border-b-4 border-indigo-400 placeholder:text-zinc-500 placeholder:text-sm outline-0 h-36 resize-none'
+                        ></textarea>
+                    </form>
+                </div>
+            </section>
         </main>
     );
 }
