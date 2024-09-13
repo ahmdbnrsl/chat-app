b40b5b49c87a062e8974cb86b5a4c79b60429d7d
abrazax56 <ahmadbenirusli@gmail.com>
2024-09-13 10:07:11 +0700

    chat-app: add home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,14 +8,14 @@ export default function Home() {
     return (
         <div
             onClick={() => console.log('Hello World!')}
-            className='h-screen w-screen bg-zinc-950 p-6 flex justify-center items-center'
+            className='h-screen w-screen bg-zinc-950 p-6 flex flex-col justify-center items-center gap-3'
         >
             <h1 className='text-zinc-300 text-4xl font-bold font-serif'>
                 VB CHAT
             </h1>
             <Link
                 href={session?.user ? '/chat' : '/login'}
-                className='w-fit rounded-full bg-zinc-400 text-base px-5 py-1 outline-0'
+                className='w-fit rounded-full bg-zinc-200 text-base px-5 w-full max-w-md py-1 outline-0 text-zinc-950'
             >
                 {session?.user ? 'Start Chat' : 'Login'}
             </Link>
